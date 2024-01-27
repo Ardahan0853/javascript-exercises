6bb18bdedc92be173ad43b1a90ce17e9bfa8551c
Ardahan0853 <ardahan08530853@hotmail.com>
2024-01-27 17:45:10 +0300

    fixed bugs

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,26 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    const reversedString = string.split("").reverse().join("").toLowerCase()
+    const regexString = reversedString.replace(/[^a-z0-9]/g, "")
+    const realString = string.toLowerCase().replace(/[^A-z0-9]/g, "");
+
+    return regexString ==  realString
+
+
+
+    // const lowerCase = string.toLowerCase();
+    // const regexCase = lowerCase.replace(/[^a-z0-9]/g, "")
+    // const splittedCase = regexCase.split("")
+    // const reversedCase = splittedCase.reverse()
+    // const joinedCase = reversedCase.join("")
+
+    // return(`${lowerCase}\n ${regexCase} \n ${splittedCase} \n ${reversedCase} \n ${joinedCase} \n ${string.toLowerCase()}`)
+    // return joinedCase == regexCase
+
+    
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+// .string.split("").reduce((a , b) => b + a, "").toLowerCase()
