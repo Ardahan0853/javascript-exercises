a7f12d6680c98470145db7bddc1a5428a32091dd
Ardahan0853 <ardahan08530853@hotmail.com>
2024-01-28 15:05:40 +0300

    it is getting kinda messy

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(person) {
+    
+    const newArr = person.map(item => item.yearOfDeath - item.yearOfBirth)
+    const newArrSorted = newArr.sort((a,b) => b - a)
+    for(let i = 0; i < person.length; i++){
+      if(newArrSorted[0] === (person[i].yearOfDeath - person[i].yearOfBirth)){
+        return person[i]
+    }else if(!person[i].yearOfDeath){
+        return person[i]
+    }
+    }
 };
 
 // Do not edit below this line
